0269d65cf5fd249bcd6166af7537ee9eef4436d8
James Addison <jay@jp-hosting.net>
2022-07-23 19:15:55 +0100

    Populate film data

--- film_calendar.py
+++ film_calendar.py
@@ -63,12 +63,60 @@ class Film(object):
 
 
 # Screenings, sorted by start time
-screenings = [
-    Film(begin="2022-08-13 14:00", running_time=120, venue="CAM", title="First"),
-    Film(begin="2022-08-13 18:00", running_time=60, venue="FLH", title="Second"),
-    Film(begin="2022-08-13 19:00", running_time=120, venue="FLH", title="Third"),
-    Film(begin="2022-08-13 19:00", running_time=120, venue="CAM", title="Fourth"),
-]
+screenings = sorted(
+    [
+        Film(begin="2022-08-20 19:00", running_time=96, venue="VUE", title="After Yang"),
+
+        Film(begin="2022-08-16 16:30", running_time=100, venue="VUE", title="Fogaréu"),
+        Film(begin="2022-08-17 19:00", running_time=100, venue="FLH", title="Fogaréu"),
+
+        Film(begin="2022-08-16 20:35", running_time=101, venue="FLH", title="Leonor Will Never Die"),
+        Film(begin="2022-08-18 15:30", running_time=101, venue="VUE", title="Leonor Will Never Die"),
+
+        Film(begin="2022-08-15 21:00", running_time=78, venue="EVR", title="LOLA"),
+        Film(begin="2022-08-19 16:00", running_time=78, venue="VUE", title="LOLA"),
+
+        Film(begin="2022-08-17 18:15", running_time=87, venue="VUE", title="Full Time"),
+        Film(begin="2022-08-18 16:00", running_time=87, venue="FLH", title="Full Time"),
+
+        Film(begin="2022-08-18 21:35", running_time=109, venue="VUE", title="Special Delivery"),
+        Film(begin="2022-08-19 16:20", running_time=109, venue="VUE", title="Special Delivery"),
+
+        Film(begin="2022-08-15 19:00", running_time=83, venue="CAM", title="Anonymous Club"),
+        Film(begin="2022-08-17 21:30", running_time=83, venue="VUE", title="Anonymous Club"),
+
+        Film(begin="2022-08-17 15:50", running_time=115, venue="VUE", title="Hallelujah"),
+        Film(begin="2022-08-20 16:50", running_time=115, venue="FLH", title="Hallelujah"),
+
+        Film(begin="2022-08-13 14:00", running_time=85, venue="VUE", title="The Territory"),
+        Film(begin="2022-08-19 18:00", running_time=85, venue="EVR", title="The Territory"),
+
+        Film(begin="2022-08-17 20:35", running_time=117, venue="VUE", title="The Forgiven"),
+
+        Film(begin="2022-08-18 19:00", running_time=114, venue="VUE", title="The Score"),
+        Film(begin="2022-08-20 13:30", running_time=114, venue="FLH", title="The Score"),
+
+        Film(begin="2022-08-15 21:10", running_time=104, venue="VUE", title="AEIOU"),
+        Film(begin="2022-08-16 14:00", running_time=104, venue="FLH", title="AEIOU"),
+
+        Film(begin="2022-08-14 17:30", running_time=112, venue="VUE", title="Axiom"),
+        Film(begin="2022-08-16 11:30", running_time=112, venue="VUE", title="Axiom"),
+
+        Film(begin="2022-08-14 14:15", running_time=97, venue="VUE", title="Phantom Project"),
+        Film(begin="2022-08-15 21:20", running_time=97, venue="CAM", title="Phantom Project"),
+
+        Film(begin="2022-08-13 18:30", running_time=180, venue="FLH", title="The Plains"),
+
+        Film(begin="2022-08-16 18:30", running_time=56, venue="VUE", title="Shadow"),
+
+        Film(begin="2022-08-14 15:40", running_time=68, venue="FLH", title="EIFF New Visions"),
+
+        Film(begin="2022-08-13 15:30", running_time=79, venue="FLH", title="Scotland's Voices"),
+
+        Film(begin="2022-08-14 11:30", running_time=60, venue="FLH", title="The Making of A Bear Named Wojtek"),
+    ],
+    key=lambda f: f.begin,
+)
 n = len(screenings)
 
 model = CpModel()
